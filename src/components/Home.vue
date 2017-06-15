966310d548ac3b6e4fcc4c555ed6753217eacb98
Soar <85624529@qq.com>
2017-06-15 15:17:21 +0800

    [feature] add scss

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <Nav/> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="nav">
+    <ul>
+      <li>    
+        <a href="/test">解决方案</a>
+      </li>
+      <li>    
+        <a href="/test">产品中心</a>
+      </li>
+      <li>    
+        <a href="/test">关于我们</a>
+      </li>
+ 
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nav',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.nav{
+  background: #ea5413;
+  height: 60px;
+}
+ul {
+  margin: 0;
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #fff;
+  text-decoration: none;
+}
+</style>
