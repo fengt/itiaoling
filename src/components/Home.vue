71ab7b76d059775d22b3eadf2b989c32e3e92125
fhalcyon <fhalcyon@gmail.com>
2017-06-26 17:20:18 +0800

    [optimize] add content of home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <div class="head-wraper">
+    <div class="head-wrapper">
       <Nav/> 
       <div class="container">
         <div class="landing">
@@ -11,7 +11,67 @@
 
     </div>
 
-    <div class="customer-wraper">
+
+
+    <div class="itl-wrapper">
+      <div class="itl-row">
+        <div class="container">
+          <div class="row">
+            <div class="col-sx-12 col-sm-6 col-md-6">
+              <div class="intro">
+                <h2>安全认证</h2>
+                <p>AAAAA级物流公司--天地华宇集团进行严格的司机及车辆审核认证，提供整车货物保险、正规发票，全程在线货物跟踪服务，为您的货物保价护航。</p>
+              </div>
+            </div>
+            <div class="col-sx-12 col-sm-6 col-md-6 feature-intro-image">
+              <img src="../assets/images/conversion.gif">
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="itl-row">
+        <div class="container">
+          <div class="row">
+            <div class="col-sx-12 col-sm-6 col-md-6 text-right">
+              <div class="intro">
+                <h2>安全认证</h2>
+                <p>AAAAA级物流公司--天地华宇集团进行严格的司机及车辆审核认证，提供整车货物保险、正规发票，全程在线货物跟踪服务，为您的货物保价护航。</p>
+              </div>
+            </div>
+            <div class="col-sx-12 col-sm-6 col-md-6 feature-intro-image">
+              <img src="../assets/images/conversion.gif">
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="itl-row">
+        <div class="container">
+          <div class="row">
+            <div class="col-sx-12 col-sm-6 col-md-6">
+              <div class="intro">
+                <h2>安全认证</h2>
+                <p>AAAAA级物流公司--天地华宇集团进行严格的司机及车辆审核认证，提供整车货物保险、正规发票，全程在线货物跟踪服务，为您的货物保价护航。</p>
+              </div>
+            </div>
+            <div class="col-sx-12 col-sm-6 col-md-6 feature-intro-image">
+              <img src="../assets/images/conversion.gif">
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+
+
+
+
+
+    <div class="customer-wrapper">
 	    <div class="container">
 	      <div class="customer-header">
 	      	<h1>客户案例</h1>
@@ -74,7 +134,7 @@ export default {
 .landing{
   margin-top: 200px;
 }
-.head-wraper{
+.head-wrapper{
   min-height: 700px;
   background-image: url(../assets/images/landing.png), linear-gradient(120deg, #ea5413, #F39800);
   background-size: cover;
@@ -82,7 +142,59 @@ export default {
 }
 
 
-.customer-wraper{
+.itl-wrapper{
+  .itl-row{
+    padding: 40px 0;
+    overflow: hidden;
+    position: relative;
+    .row > div{
+      position: relative;
+      height: 400px;
+      @media (max-width: 767px){
+        height: 250px;
+      }
+    }
+    .intro{
+      position: absolute;
+      bottom: 0;
+      margin: auto;
+      height: 300px;
+      right: 0;
+      left: 22%;
+      top: 11%;
+      text-align: left;
+      max-width: 360px;
+      line-height: 2em;
+      @media (max-width: 767px){
+        height: 250px;
+        text-align: center;
+        margin: 30px auto;
+        padding: 0 30px;
+        left: 0;
+        top: -20%;
+        max-width: 90%;
+      }
+    }
+    .text-right{
+      float: right;
+      @media (max-width: 575px){
+        float: none;
+      }
+    }
+    p{
+      color: #65697f;
+    }
+  }
+
+
+  img{
+    width: 80%;
+  }
+
+}
+
+
+.customer-wrapper{
 	min-height: 500px;
 }
 .customer-list-all{
