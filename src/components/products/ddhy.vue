ccc054d08040e27533b268b33f0422cc4e415215
fhalcyon <fhalcyon@gmail.com>
2017-06-27 17:53:25 +0800

    [optimize] replace pictures of ddhy page

--- src/components/products/ddhy.vue
+++ src/components/products/ddhy.vue
@@ -59,7 +59,7 @@
               </div>
             </div>
             <div class="col-sx-12 col-sm-6 col-md-6 feature-intro-image">
-              <img src="../../assets/images/products/ddhy/dodo_show.png">
+              <img src="../../assets/images/products/ddhy/safe_pic.png">
             </div>
           </div>
         </div>
@@ -75,7 +75,7 @@
               </div>
             </div>
             <div class="col-sx-12 col-sm-6 col-md-6 feature-intro-image">
-              <img src="../../assets/images/products/ddhy/dodo_show.png">
+              <img src="../../assets/images/products/ddhy/flow_pic.png">
             </div>
           </div>
         </div>
@@ -91,7 +91,7 @@
               </div>
             </div>
             <div class="col-sx-12 col-sm-6 col-md-6 feature-intro-image">
-              <img src="../../assets/images/products/ddhy/dodo_show.png">
+              <img src="../../assets/images/products/ddhy/benefit_pic.png">
             </div>
           </div>
         </div>
@@ -196,7 +196,7 @@ export default {
       height: 300px;
       right: 0;
       left: 22%;
-      top: 11%;
+      // top: 11%;
       text-align: left;
       max-width: 360px;
       line-height: 2em;
@@ -206,7 +206,7 @@ export default {
         margin: 30px auto;
         padding: 0 30px;
         left: 0;
-        top: -20%;
+        // top: -20%;
         max-width: 90%;
       }
     }
