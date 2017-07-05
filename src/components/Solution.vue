d56de9d481ec6b2066774649174a91e0dce619e2
fhalcyon <fhalcyon@gmail.com>
2017-07-05 10:30:55 +0800

    [optimize] solution page optimize

--- src/components/Solution.vue
+++ src/components/Solution.vue
@@ -23,128 +23,128 @@
 
             <div class="row">
               <div class="col-md-4 col-xs-6">
-                <img class="solution-item-img" src="../assets/images/products/common/safe.png" alt="solution-icon">
+                <img class="solution-item-img" src="../assets/images/solution/wms.png" alt="solution-icon">
                 <h2>仓配一体</h2>
               </div>
               <div class="col-md-4 col-xs-6">
-                <img class="solution-item-img" src="../assets/images/products/common/safe.png" alt="solution-icon">
+                <img class="solution-item-img" src="../assets/images/solution/delivery.png" alt="solution-icon">
                 <h2>快递</h2>
               </div>
               <div class="col-md-4 col-xs-6">
-                <img class="solution-item-img" src="../assets/images/products/common/safe.png" alt="solution-icon">
+                <img class="solution-item-img" src="../assets/images/solution/express.png" alt="solution-icon">
                 <h2>快运</h2>
+              </div>
+              <div class="col-md-6 col-xs-6 visible-md visible-xs">
+                <img class="solution-item-img" src="../assets/images/solution/O2O.png" alt="solution-icon">
+                <h2>O2O</h2>
               </div>
             </div>
 
             <div class="row">
-              <div class="col-md-4 col-xs-6">
-                <img class="solution-item-img" src="../assets/images/products/common/safe.png" alt="solution-icon">
+              <div class="col-md-6 col-xs-6 hidden-md hidden-xs">
+                <img class="solution-item-img" src="../assets/images/solution/O2O.png" alt="solution-icon">
                 <h2>O2O</h2>
               </div>
-              <div class="col-md-4 col-xs-6">
-                <img class="solution-item-img" src="../assets/images/products/common/safe.png" alt="solution-icon">
+              <div class="col-md-6 col-xs-6">
+                <img class="solution-item-img" src="../assets/images/solution/mobile.png" alt="solution-icon">
                 <h2>移动</h2>
               </div>
             </div>
 
           </div>
-
         </div>
+      </div>
         
 
 
         <div class="main-content">
-
+        <div class="container">
 
           <div class="solt-intro-item clearfix">
-            <div class="intro-img intro-left">
-              <img src="../assets/images/solution-example.png" alt="">
-            </div>
-            <div class="intro-content">
-              <span class="intro-title">仓配一体</span>
-              <p>在行业领先仓储信息系统以及完善的仓储服务基础上，整合配送业务、配送资源，并结合强大的系统承载能力，打通了商品从存储到订单信息流入、商品拣货配货、打包再到分拣出库、末端配送，以及商品逆向流通的一整套流程和服务。用计算机信息系统最大限度的支持仓配一体管理信息的处理，充分发挥人机协同的功效，提高效率，减少错误，使管理人员可以及时地了解每一个环节的状况和问题。
-  </p><p>
-  跳羚科技自主研发的仓储物流系统和配送平台的系统搭建始于2007年，专注于电子商务模式下的仓储配送。每日商品出库750万件、系统500万单、存储5000万件；库存差异率低于万分之六；经历多年双十一洗礼，具备超强的大促应对能力。
-  </p><p>
-  长期的运营积累了丰富的经验，已经形成了规模效应，可大大提高运营效率，降低运营成本。可为电商和传统企业提供从订单生产处理、库存管理，到退货逆向物流、作业报表监控、流程服务持续改进、结算对账、多平台信息对接等的系统解决方案和服务，并为转型互联网的传统企业提供整套互联网仓配一体解决方案。可给客户提供高标准、高质量、多样化、全方位的个性化服务。</p>
-            </div>
+          <div class="intro-img intro-left">
+            <img src="../assets/images/solution-example.png" alt="">
+          </div>
+          <div class="intro-content">
+            <span class="intro-title">仓配一体</span>
+            <p>在行业领先仓储信息系统以及完善的仓储服务基础上，整合配送业务、配送资源，并结合强大的系统承载能力，打通了商品从存储到订单信息流入、商品拣货配货、打包再到分拣出库、末端配送，以及商品逆向流通的一整套流程和服务。用计算机信息系统最大限度的支持仓配一体管理信息的处理，充分发挥人机协同的功效，提高效率，减少错误，使管理人员可以及时地了解每一个环节的状况和问题。
+</p><p>
+跳羚科技自主研发的仓储物流系统和配送平台的系统搭建始于2007年，专注于电子商务模式下的仓储配送。每日商品出库750万件、系统500万单、存储5000万件；库存差异率低于万分之六；经历多年双十一洗礼，具备超强的大促应对能力。
+</p><p>
+长期的运营积累了丰富的经验，已经形成了规模效应，可大大提高运营效率，降低运营成本。可为电商和传统企业提供从订单生产处理、库存管理，到退货逆向物流、作业报表监控、流程服务持续改进、结算对账、多平台信息对接等的系统解决方案和服务，并为转型互联网的传统企业提供整套互联网仓配一体解决方案。可给客户提供高标准、高质量、多样化、全方位的个性化服务。</p>
           </div>
         </div>
 
 
 
-          <div class="solt-intro-item clearfix">
-            <div class="intro-img intro-right">
-              <img src="../assets/images/solution-example.png" alt="">
-            </div>
-            <div class="intro-content">
-              <span class="intro-title">快递</span>
-              <p>快递是物流的一种形态，与传统物流不同的是：物流的核心要素是仓储、运输和包装；而快递则是一种个性化精益物流服务，更重视速度。为了“快”，它必须综合运用各种运输方式，辅以网络化的区域机构。快递主要面向散单，其特点是物品相对较小、品种多、中间经过的环节多。所以相对于传统物流行业来说快递对于系统的支持要求是更加多样化的，支持更多的个性化的服务。
-  </p><p>
-  跳羚科技独立开发的快递解决方案拥有整套IT运维支持解决方案，为众电商客户及合作伙伴在双十一期间提供了有力的技术支撑。采用EDI和DMS双数据库服务系统，具备海量数据分析能力及运行承受能力，并实现双系统自动切换，全面保障了数据安全稳定运行。配送员专用的飞鱼APP，在支撑配送员收、签、派、查等基本业务的基础上，加入移动支付、搜索、定位等扩展功能，提高了配送效率与运单妥投率。同时，系统平台还实现了对全国各区域分拣单量、配送单量及滞留单量的实时监控反馈并进行目标预警，全面进入到快递生产各个环节，最大限度的将问题在技术层面解决，以此减少人为因素的影响。</p>
-            </div>
+        <div class="solt-intro-item clearfix">
+          <div class="intro-img intro-right">
+            <img src="../assets/images/solution-example.png" alt="">
           </div>
-
-
-
-          <div class="solt-intro-item clearfix">
-            <div class="intro-img intro-left">
-              <img src="../assets/images/solution-example.png" alt="">
-            </div>
-            <div class="intro-content">
-              <span class="intro-title">快运</span>
-              <p>快运解决方案是跳羚科技基于快运系统，长期对快运现状的调研，针对多网点和联盟型物流公司或集团定制的物流管理系统，研发出有特色的快运整体解决方案，解决了市场上主要针对快运的系统的一些问题。该方案在继承了快运系统全面而实用的功能和方便使用、快速配置等特征的基础上，结合多网点或联盟型物流公司的实际业务需要。不同的公司可以根据自己实际的流程进行一些变更。
-  </p><p>
-  可提供保价运输、安全包装、代收货款、签收单返单等增值服务提供个性化系统服务，可承运您的超重、异型等特殊货物针对性定制信息解决方案。
-  </p><p>
-  跳羚科技快运解决方案为企业客户提供高端公路快运服务的IT解决方案，它以“准时、安全、优质服务”作为核心价值，高度的时效性和准确性成为中国公路快运IT系统的首选方案。</p>
-            </div>
+          <div class="intro-content">
+            <span class="intro-title">快递</span>
+            <p>快递是物流的一种形态，与传统物流不同的是：物流的核心要素是仓储、运输和包装；而快递则是一种个性化精益物流服务，更重视速度。为了“快”，它必须综合运用各种运输方式，辅以网络化的区域机构。快递主要面向散单，其特点是物品相对较小、品种多、中间经过的环节多。所以相对于传统物流行业来说快递对于系统的支持要求是更加多样化的，支持更多的个性化的服务。
+</p><p>
+跳羚科技独立开发的快递解决方案拥有整套IT运维支持解决方案，为众电商客户及合作伙伴在双十一期间提供了有力的技术支撑。采用EDI和DMS双数据库服务系统，具备海量数据分析能力及运行承受能力，并实现双系统自动切换，全面保障了数据安全稳定运行。配送员专用的飞鱼APP，在支撑配送员收、签、派、查等基本业务的基础上，加入移动支付、搜索、定位等扩展功能，提高了配送效率与运单妥投率。同时，系统平台还实现了对全国各区域分拣单量、配送单量及滞留单量的实时监控反馈并进行目标预警，全面进入到快递生产各个环节，最大限度的将问题在技术层面解决，以此减少人为因素的影响。</p>
           </div>
+        </div>
 
 
 
-          <div id="O2O" class="solt-intro-item clearfix">
-            <div class="intro-img intro-right">
-              <img src="../assets/images/solution-example.png" alt="">
-            </div>
-            <div class="intro-content">
-              <span class="intro-title">O2O</span>
-              <p>从李克强总理在中关村创业大街喝的那杯咖啡算起，“互联网+”的概念提出已经近一年了，O2O发展在中国也经过了几个年头的沉淀。
-  </p><p>
-  2010年开始，率先开始互联网转型的国内第一批企业在未来的1年内，将进入到互联网转型成果的批量产出期，线上业务进入到持续骤增的时间窗口，线上线下的融合也将迎来爆发性的增长；为全渠道市场扩散提供了有益的借鉴，并表明发展方向。
-  </p><p>
-  O2O全渠道转型升级已成为行业共识。虽然在过去的一年里O2O行业从餐饮到社区等一系列行业的O2O都已经尸横遍野，但是更多的传统行业认识已经认识到互联网时代已经到来，O2O覆盖着我们生活的方方面面，传统行业在如此巨大的冲击下，如何让互联网成为队友而非对手，将线上互联网与线下传统行业完美结合。
-  </p><p>
-  对于向互联网商业转型、O2O全渠道演进，新的一年转型升级会进一步提速，纵深推进，各种多元化跨界、特色化经营、更多形式的触网、更多形态的O2O、全渠道会继续涌现，某些创新将在运营中成熟定型，释放出日益强大的功能。
-  </p><p>
-  跳羚科技为各类行业客户提供应用开发、管理、咨询等一体化服务，帮助行业客户建立线上线下融合的全渠道运营体系，促进传统行业向互联网+转型。</p>
-            </div>
+        <div class="solt-intro-item clearfix">
+          <div class="intro-img intro-left">
+            <img src="../assets/images/solution-example.png" alt="">
           </div>
-
-
-
-          <div class="solt-intro-item clearfix">
-            <div class="intro-img intro-left">
-              <img src="../assets/images/solution-example.png" alt="">
-            </div>
-            <div class="intro-content">
-              <span class="intro-title">移动</span>
-              <p>随着移动互联网的兴起， 移动化已经是进一步推动互联网飞速前进重要关键词。越来越多的客户注意到移动互联网的影响力。互联网从PC端向移动端的迅速延伸使移动端成为互联网最重要的入口，但在我们热衷于谈论移动互联网的时候，更要了解当前移动解决方案为企业带来的影响。
-  </p><p>
-  从终端上来说，移动设备的采购价格往往比PC低很多，将能节省大量的采购成本。而且，绝大多数组织员工都会随身携带智能手机等移动设备，只要组织针对这些设备进行移动化改造，很容易就能将这些设备转化为工作终端，成本也会得到极大的降低。
-  </p><p>
-  毋庸置疑，移动互联网是未来的发展方向，其用户量更大、活跃性更高、蕴藏的商业价值也越大，是组织抢占产业“风口”的必争之地。因此，及早实现向移动互联网的转型不仅有助于组织提高生产力，还有利于组织提升在未来互联网经济中的竞争力。
-  </p><p>
-  跳羚科技致力于为客户提供APP（移动应用程序）、PDA（数据采集器）、POS（移动支付终端）、微信等移动应用解决方案。为企业量身打造移动办公策略，具备高可用、易拓展、低成本、高安全的特性，全面提升企业竞争力。</p>
-            </div>
+          <div class="intro-content">
+            <span class="intro-title">快运</span>
+            <p>快运解决方案是跳羚科技基于快运系统，长期对快运现状的调研，针对多网点和联盟型物流公司或集团定制的物流管理系统，研发出有特色的快运整体解决方案，解决了市场上主要针对快运的系统的一些问题。该方案在继承了快运系统全面而实用的功能和方便使用、快速配置等特征的基础上，结合多网点或联盟型物流公司的实际业务需要。不同的公司可以根据自己实际的流程进行一些变更。
+</p><p>
+可提供保价运输、安全包装、代收货款、签收单返单等增值服务提供个性化系统服务，可承运您的超重、异型等特殊货物针对性定制信息解决方案。
+</p><p>
+跳羚科技快运解决方案为企业客户提供高端公路快运服务的IT解决方案，它以“准时、安全、优质服务”作为核心价值，高度的时效性和准确性成为中国公路快运IT系统的首选方案。</p>
           </div>
+        </div>
 
 
 
+        <div id="O2O" class="solt-intro-item clearfix">
+          <div class="intro-img intro-right">
+            <img src="../assets/images/solution-example.png" alt="">
+          </div>
+          <div class="intro-content">
+            <span class="intro-title">O2O</span>
+            <p>从李克强总理在中关村创业大街喝的那杯咖啡算起，“互联网+”的概念提出已经近一年了，O2O发展在中国也经过了几个年头的沉淀。
+</p><p>
+2010年开始，率先开始互联网转型的国内第一批企业在未来的1年内，将进入到互联网转型成果的批量产出期，线上业务进入到持续骤增的时间窗口，线上线下的融合也将迎来爆发性的增长；为全渠道市场扩散提供了有益的借鉴，并表明发展方向。
+</p><p>
+O2O全渠道转型升级已成为行业共识。虽然在过去的一年里O2O行业从餐饮到社区等一系列行业的O2O都已经尸横遍野，但是更多的传统行业认识已经认识到互联网时代已经到来，O2O覆盖着我们生活的方方面面，传统行业在如此巨大的冲击下，如何让互联网成为队友而非对手，将线上互联网与线下传统行业完美结合。
+</p><p>
+对于向互联网商业转型、O2O全渠道演进，新的一年转型升级会进一步提速，纵深推进，各种多元化跨界、特色化经营、更多形式的触网、更多形态的O2O、全渠道会继续涌现，某些创新将在运营中成熟定型，释放出日益强大的功能。
+</p><p>
+跳羚科技为各类行业客户提供应用开发、管理、咨询等一体化服务，帮助行业客户建立线上线下融合的全渠道运营体系，促进传统行业向互联网+转型。</p>
+          </div>
+        </div>
 
+
+
+        <div class="solt-intro-item clearfix">
+          <div class="intro-img intro-left">
+            <img src="../assets/images/solution-example.png" alt="">
+          </div>
+          <div class="intro-content">
+            <span class="intro-title">移动</span>
+            <p>随着移动互联网的兴起， 移动化已经是进一步推动互联网飞速前进重要关键词。越来越多的客户注意到移动互联网的影响力。互联网从PC端向移动端的迅速延伸使移动端成为互联网最重要的入口，但在我们热衷于谈论移动互联网的时候，更要了解当前移动解决方案为企业带来的影响。
+</p><p>
+从终端上来说，移动设备的采购价格往往比PC低很多，将能节省大量的采购成本。而且，绝大多数组织员工都会随身携带智能手机等移动设备，只要组织针对这些设备进行移动化改造，很容易就能将这些设备转化为工作终端，成本也会得到极大的降低。
+</p><p>
+毋庸置疑，移动互联网是未来的发展方向，其用户量更大、活跃性更高、蕴藏的商业价值也越大，是组织抢占产业“风口”的必争之地。因此，及早实现向移动互联网的转型不仅有助于组织提高生产力，还有利于组织提升在未来互联网经济中的竞争力。
+</p><p>
+跳羚科技致力于为客户提供APP（移动应用程序）、PDA（数据采集器）、POS（移动支付终端）、微信等移动应用解决方案。为企业量身打造移动办公策略，具备高可用、易拓展、低成本、高安全的特性，全面提升企业竞争力。</p>
+          </div>
+        </div>
 
       </div>
 
+    </div>
     </div>
 
 
@@ -178,6 +178,14 @@ export default {
     background-position: 85% 60%;
     @media (max-width: 767px){
       background-position: 50% 95%;
+      background-size: 30%;
+    }
+    @media (max-width: 576px){
+      background-position: 50% 95%;
+      background-size: 30%;
+    }
+    @media (max-width: 430px){
+      background-position: 50% 95%;
       background-size: 40%;
     }
   }
@@ -200,8 +208,10 @@ export default {
 }
 
 
+
+/*solution icon display css*/
 .solution-icon-all{
-  margin: 50px 0 80px 0;
+  margin-top: 70px;
   .desc{
     font-size: 24px;
     color: #65697f;
@@ -211,6 +221,10 @@ export default {
     margin-bottom: 60px;
   }
   .container{
+    width: 80%;
+    .row {
+      padding-bottom: 50px;
+    }
     h2{
       font-size: 20px;
       font-weight: 400;
@@ -218,37 +232,66 @@ export default {
       margin-bottom: 30px;
     }
   }
+  img{
+    width: 120px;
+  }
 }
 
 
+/*详情介绍css*/
+.main-content {
+  .container {
+    width: 80%;
+    margin-bottom: 50px;
+  }
+}
 .solt-intro-item{
-  margin: 10%;
+  margin: 8%;
+  @media (max-width: 1200px){
+    margin: auto;
+  }
   .intro-left{
     float: left;
     margin-right: 80px;
+    @media (max-width: 768px){
+      float: none;
+    }
     @media (max-width: 575px){
       margin-right: 0;
     }
   }
   .intro-right{
     float: right;
+    @media (max-width: 768px){
+      float: none;
+    }
   }
   .intro-title{
     font-size: 24px;
     font-weight: bold;
     text-align: left;
   }
-  p{
-    font-size: 16px;
-    text-align: left;
-    text-indent: 30px;
-    line-height: 2;
-    color: #383963;
-  }
   img{
-    width: 100%;
+    width: 90%;
     padding: 10%;
+    @media (max-width: 768px){
+      width: 70%;
+    }
+    @media (max-width: 576px){
+      width: 80%;
+    }
   }
+  .intro-content{
+    margin: auto;
+    p{
+      font-size: 16px;
+      text-align: left;
+      text-indent: 30px;
+      line-height: 2;
+      color: #383963;
+    }
+  }
+  
 }
 
 
